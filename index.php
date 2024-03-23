4b93e38b27bb5e6f5f81ddbe16585b9862823f32
Teker <iamahaver@of.sex>
2024-03-23 12:30:14 -0600

    initial commit

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="./style.css">
+        <title>Mein Raum</title>
+    </head>
+    <body>
+        <div id="header">
+            <h1>Mein Raum</h1> 
+            <p class="italic">A modern mySpace</p>
+            <?php foreach ($links as $link): ?>
+                <p></p>
+            <?php endforeach; ?>
+        </div>
+    </body>
+</html>
